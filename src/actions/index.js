78acbeb557275b4b2c848984e43097edf6ad81c8
ngani faith <nganifaith@gmail.com>
2021-09-01 12:56:29 +0100

    Add reducer and action for catergory

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const SET_MEAL = 'SET_MEAL';
 export const SET_MEAL_DETAILS = 'SET_MEAL_DETAILS';
+export const SET_CATERGORIES = 'SET_CATERGORIES';
 
 export const setMeals = (meals) => ({
   type: SET_MEAL,
@@ -9,4 +10,9 @@ export const setMeals = (meals) => ({
 export const setMealDetails = (meal) => ({
   type: SET_MEAL_DETAILS,
   payload: meal,
+});
+
+export const setCAetrgories = (catergories) => ({
+  type: SET_CATERGORIES,
+  payload: catergories,
 });
